10609bf66342e10bdb337e98919118cf38218b9d
LoveNyabi <r91090701@gmail.com>
2023-05-21 23:24:01 +0800

    現在可以決定要不要使用抽牌抽到有得出牌為止的規則

--- Utils/Game/Rule/RuleStrategy/MustThrowCard/MustThrowCard.js
+++ Utils/Game/Rule/RuleStrategy/MustThrowCard/MustThrowCard.js
@@ -1,7 +1,6 @@
 const checker = require("../../RuleChecker")
 module.exports = class MustThrowCard{
     execute(game,player,newCard){
-        console.log("??")
         if(checker.checkOneCardIsValid(game,newCard)){
             player.isDrawed=true
         }
--- Utils/Game/Rule/RuleStrategy/MustThrowCard/NotMustThrowCard.js
+++ Utils/Game/Rule/RuleStrategy/MustThrowCard/NotMustThrowCard.js
@@ -1,7 +1,6 @@
 const checker = require("../../RuleChecker")
 module.exports = class NotMustThrowCard{
     execute(game,player,newCard){
-        console.log("???")
         player.isDrawed=true
         if(!checker.checkOneCardIsValid(game,newCard)){
             game.endRound()
