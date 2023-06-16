26d340cf82d06c275060d2b5746bad53b4a87535
LoveNyabi <r91090701@gmail.com>
2023-06-16 15:22:09 +0800

    完成

--- public/javascripts/RoomPage/Events/PlayerJoinEvent.js
+++ public/javascripts/RoomPage/Events/PlayerJoinEvent.js
@@ -3,11 +3,7 @@ class PlayerJoinEvent extends SocketEvent{
     constructor(){
         super('PlayerJoinEvent')//當玩家加入該房間時觸發
         this.handler=(data)=>{
-<<<<<<< HEAD
-            $(`#name${data.number}`).val(data.name)
-=======
             $(`.name${data.number}`).text(data.name)
->>>>>>> dev
         }
         SocketEvent.events.push(this)
     }
